ad4952e2d23adcb4d25d4e8c6633a08e66e4a2ee
AlyssaAragon <alyssaaragon002@gmail.com>
2024-11-12 13:38:19 -0800

    Fixed migration issues and updated model references to use custom user model

--- tasteBudsDjango/tastebuds/admin.py
+++ tasteBudsDjango/tastebuds/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Diet, Recipe, AllRecipe, Favorite, Partner
+from .models import Diet, Recipe, AllRecipe, Favorite, Partner, CustomUser
 
 # Register your models here.
+admin.site.register(CustomUser)
+
 admin.site.register(Diet)
 admin.site.register(Recipe)
 admin.site.register(AllRecipe)
--- tasteBudsDjango/tastebuds/models.py
+++ tasteBudsDjango/tastebuds/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import User
 
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.conf import settings 
 class Diet(models.Model):
     name = models.CharField(max_length=255, unique=True)
 
@@ -10,7 +11,7 @@ class Diet(models.Model):
 class Recipe(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     diets = models.ManyToManyField(Diet, through='RecipeDiet') 
 
@@ -35,15 +36,48 @@ class AllRecipe(models.Model):
         return self.name
 
 class Favorite(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
 
     class Meta:
         unique_together = ('user', 'recipe')
 
 class Partner(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='partners')
-    partner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='partner_of')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='partners')
+    partner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='partner_of')
 
     class Meta:
         unique_together = ('user', 'partner')
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, username, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, username, password, **extra_fields)
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=150, unique=True)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.username
