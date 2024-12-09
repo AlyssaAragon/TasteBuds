76aaf85ba186d572c418c0362c60939d78f19f62
Alicia Chiang <aliciawchiang@gmail.com>
2024-12-09 06:13:11 -0800

    maintabview

--- TasteBudsXcode/TasteBuds/TasteBuds/Core/TabBar/MainTabView.swift
+++ TasteBudsXcode/TasteBuds/TasteBuds/Core/TabBar/MainTabView.swift
@@ -14,13 +14,13 @@ struct MainTabView: View {
                 .tabItem{ Image(systemName: "flame")}
                 .tag(0)
             
-            Text("Search View")
-                .tabItem{ Image(systemName: "magnifyingglass")}
-                .tag(1)
-            
-            Text("Inbox View")
-                .tabItem{ Image(systemName: "bubble")}
-                .tag(2)
+//            Text("Search View")
+//                .tabItem{ Image(systemName: "magnifyingglass")}
+//                .tag(1)
+//            
+//            Text("Inbox View")
+//                .tabItem{ Image(systemName: "bubble")}
+//                .tag(2)
             
             Text("Profile View")
                 .tabItem{ Image(systemName: "person")}
