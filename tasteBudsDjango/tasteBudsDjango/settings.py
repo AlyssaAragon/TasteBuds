9fec301c8f19406910ed6be3c09e5a6d7f2e7668
AlyssaAragon <alyssaaragon002@gmail.com>
2025-02-28 17:46:18 -0800

    userauth settings

--- tasteBudsDjango/tasteBudsDjango/settings.py
+++ tasteBudsDjango/tasteBudsDjango/settings.py
@@ -30,7 +30,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     '3.134.252.214',  # public aws IPv4 address
     'localhost',
-    '127.0.0.1', # zach said not to use this one '127.0.0.1',
+    #'127.0.0.1',  zach said not to use this one '127.0.0.1',
     '0.0.0.0',
     'tastebuds-db.unr.dev', #server address
     'tastebuds.unr.dev',
