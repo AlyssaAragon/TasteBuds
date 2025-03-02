32f69ed1d000a40c4b28fc68776f991a8797b5a5
HannahHaggerty <hannahhaggerty4@gmail.com>
2025-03-02 08:10:46 +0000

    updated backend from server

--- tasteBudsDjango/tastebuds/serializers.py
+++ tasteBudsDjango/tastebuds/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import CustomUser, Recipe, Diet, RecipeDiet, SavedRecipe, UserDiet
+from .models import CustomUser, Recipe, Diet, RecipeDiet, SavedRecipe, UserDiet, Category, RecipeCategory
 from django.conf import settings 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -14,6 +14,8 @@ class RecipeSerializer(serializers.ModelSerializer):
         model = Recipe
         fields = ['id', 'name', 'ingredients', 'instructions', 'image_name', 'cleaned_ingredients']
 
+class PartnerLinkSerializer(serializers.Serializer):
+    partner_email = serializers.EmailField()
 
 class DietSerializer(serializers.ModelSerializer):
     class Meta:
@@ -23,7 +25,38 @@ class DietSerializer(serializers.ModelSerializer):
 class RecipeDietSerializer(serializers.ModelSerializer):
     class Meta:
         model = RecipeDiet
-        fields = '__all__'
+        fields = (
+            'recipedietid',
+            'recipe',        # You could also nest RecipeSerializer if needed
+            'vegetarian',
+            'vegan',
+            'gluten_free',
+            'dairy_free',
+            'nut_free',
+            'low_carb',
+            'keto',
+            'paleo',
+        )
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = (
+            'category_id',
+            'category_name',
+        )
+
+
+class RecipeCategorySerializer(serializers.ModelSerializer):
+    category = CategorySerializer(read_only=True)
+    recipe = RecipeSerializer(read_only=True)
+    class Meta:
+        model = RecipeCategory
+        fields = ('recipe', 'category')
+
+
+
+
 
 class SavedRecipeSerializer(serializers.ModelSerializer):
     class Meta:
